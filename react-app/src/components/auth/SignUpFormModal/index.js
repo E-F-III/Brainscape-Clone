ffcc01076b869703647e0f46e9bb7333ce045cec
Edward Felipe III <edomfel3@gmail.com>
2022-09-28 07:56:21 -1000

    Added styling for sign up button

--- react-app/src/components/auth/SignUpFormModal/index.js
+++ react-app/src/components/auth/SignUpFormModal/index.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Modal } from '../../../context/Modal';
 import SignUpForm from './SignUpForm';
 
+import './SignUpFormModal.css'
+
 function SignUpFormModal() {
     const [showModal, setShowModal] = useState(false);
 
     return (
         <>
-            <div onClick={() => setShowModal(true)}>Get Started</div>
+            <div id='signup-button' onClick={() => setShowModal(true)}>Get Started</div>
             {showModal && (
                 <Modal onClose={() => setShowModal(false)}>
                     <SignUpForm />
