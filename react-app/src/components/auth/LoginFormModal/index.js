4bd4a599be428f7b3962970c10bb291f1f1a2d68
Edward Felipe III <edomfel3@gmail.com>
2022-09-28 07:58:26 -1000

    Added styling for log in button

--- react-app/src/components/auth/LoginFormModal/index.js
+++ react-app/src/components/auth/LoginFormModal/index.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Modal } from '../../../context/Modal';
 import LoginForm from './LoginForm';
 
+import './LoginFormModal.css'
+
 function LoginFormModal() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <div onClick={() => setShowModal(true)}>Log In</div>
+      <div id='login-button' onClick={() => setShowModal(true)}>Log In</div>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <LoginForm />
