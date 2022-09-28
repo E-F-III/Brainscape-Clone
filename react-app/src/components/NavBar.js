4a3f92f916d596be33f8829e1f744da37be12abf
Edward Felipe III <edomfel3@gmail.com>
2022-09-28 07:38:02 -1000

    Log in form modal done, not styling. Working on NavBar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,35 @@
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+
+import LoginFormModal from './auth/LoginFormModal';
 import LogoutButton from './auth/LogoutButton';
 
+import './NavBar.css'
+
 const NavBar = () => {
+  const sessionUser = useSelector(state => state.session.user)
+
+  // let sessionLinks;
+  // if (sessionUser) {
+  //   sessionLinks = (
+
+  //   )
+  // }
+
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+      <div id='nav-bar-logo-container'>
+        <NavLink to='/' exact={true} activeClassName='active'>
+          <img id='nav-bar-logo' src='https://www.brainscape.com/assets/cms/public-views/shared/Brainscape-logo.svg'/>
+        </NavLink>
+      </div>
+      <div id=''>
+        <LoginFormModal />
+        {/* <LogoutButton /> */}
+      </div>
+
     </nav>
   );
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -5,13 +5,17 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 
+import { ModalProvider } from './context/Modal';
+
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ModalProvider>
         <App />
-      </Provider>
+      </ModalProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
