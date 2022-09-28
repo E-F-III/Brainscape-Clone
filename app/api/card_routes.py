de8ad7a648bac273bf8981bc87e2d85d8f771d15
Edward Felipe III <edomfel3@gmail.com>
2022-09-27 19:05:45 -1000

    API Route for Edit a card

--- app/api/card_routes.py
+++ app/api/card_routes.py
@@ -13,3 +13,34 @@ def validation_errors_to_error_messages(validation_errors):
         for error in validation_errors[field]:
             errorMessages.append(f'{field} : {error}')
     return errorMessages
+
+
+
+# Edit a card by id
+@card_routes.route('/<int:id>', methods=["PUT"])
+@login_required
+def update_card(id):
+    card = Card.query.get(id)
+
+    if not card:
+        return {"message": "Card could not be found", "statusCode": 404}, 404
+
+    if card.owner_id != current_user.id:
+        return {"message": "Forbidden", "statusCode": 403}, 403
+
+    form = CardForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit:
+        card.question = form.question.data
+        card.answer = form.answer.data
+
+        db.session.add(card)
+        db.session.commit()
+
+        return deck.to_dict()
+
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
+
+
+# Delete a card by id
