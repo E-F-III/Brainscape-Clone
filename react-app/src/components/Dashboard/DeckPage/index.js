4a38eb8722d33cab3433e3066ac54f269a267664
Edward Felipe III <edomfel3@gmail.com>
2022-09-29 12:18:24 -1000

    Base css for deck page. Head to feature 2 for cards rendering

--- react-app/src/components/Dashboard/DeckPage/index.js
+++ react-app/src/components/Dashboard/DeckPage/index.js
@@ -1,17 +1,66 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { NavLink, Route, Switch, useParams, useRouteMatch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, Route, Switch, useHistory, useParams, useRouteMatch } from "react-router-dom";
+import { getClassDecksThunk } from "../../../store/deck";
 
 import './DeckPage.css'
 
 function DeckPage() {
-    const { deckId } = useParams()
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const { classId, deckId } = useParams()
     const { url } = useRouteMatch()
 
+    const singleClass = useSelector(state => state.classes[Number(classId)])
     const deck = useSelector(state => state.decks[Number(deckId)])
-    console.log(deck)
-    return (
-        <h1>DECK PAGE</h1>
+
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        (async () => {
+            await dispatch(getClassDecksThunk({ classId }))
+            setIsLoaded(true)
+        })()
+    }, [dispatch])
+
+    return isLoaded && (
+        <div>
+            <div id="deck-header">
+                <div id="deck-header-top-row">
+                    <div id="class-header-top-content">
+                        <div onClick={() => history.push(`/dashboard/${classId}`)} id="class-button">
+                            <div>
+                                <ion-icon name="chevron-back-outline" />
+                            </div>
+                            <div>
+                                <img id="class-icon-button" src="https://www.brainscape.com/assets/app_icons/ugs.png" />
+
+                            </div>
+                            <div id="class-name">{singleClass.title}</div>
+                        </div>
+                        <div id="deck-header-info">
+                            <div id="deck-title">{deck.title}</div>
+                            <div>
+                                <ion-icon name="ellipsis-horizontal-outline" />
+                            </div>
+                        </div>
+                    </div>
+                    {/* Feature 2: Cards (study)
+                    <div></div> */}
+                </div>
+                <div id="deck-tabs">
+                    <div className="deck-tab-container">
+                        <NavLink className="deck-tab" to={`${url}/cards/preview`}>Preview Cards</NavLink>
+                    </div>
+                    <div className="deck-tab-container">
+                        <NavLink className="deck-tab" to={`${url}/cards/edit`}>Edit Cards</NavLink>
+                    </div>
+                    <div className="deck-tab-container">
+                        <NavLink className="deck-tab" to={`${url}/cards/browse`}>Browse Deck</NavLink>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
